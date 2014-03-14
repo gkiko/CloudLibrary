1ef481bac5faa98850de868df9a0c34e2ac1eae6
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2014-03-14 22:02:01 +0400

    renamed main class

--- src/MainService.java
+++ src/MainService.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Main
+ * Servlet implementation class MainService
  */
-@WebServlet("/Main")
-public class Main extends HttpServlet {
+@WebServlet("/MainService")
+public class MainService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Main() {
+    public MainService() {
         super();
         // TODO Auto-generated constructor stub
     }
